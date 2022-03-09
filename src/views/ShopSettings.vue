271153e07d7dbe646aa54630719e268f4717a131
Onur Altuntaş <50622451+Onurlulardan@users.noreply.github.com>
2022-03-09 17:08:02 +0300

    Add files via upload

---
+++ src/views/ShopSettings.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="section-view">
+         <div class="row row-cols-xxl-5 row-cols-lg-3 row-cols-1">
+             <my-shop/>
+             <my-shop/>
+             <my-shop/>
+             <my-shop/>
+             <my-shop/>
+             <my-shop/>
+             <my-shop/>
+        </div>
+    </section>
+</template>
+
+<script>
+import Shop from '../components/Shop.vue';
+
+export default {
+   name: 'ShopSettings',
+   components: {
+         'my-shop': Shop
+   }
+}
+
+
+</script>
+
